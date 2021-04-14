d256248650f449eed624570e82f4c9ef72a440e3
Tomer Alon <talon1@binghamton.edu>
2021-04-14 20:52:23 +0000

    about page and cucumber tests

--- features/step_definitions/welcome_steps.rb
+++ features/step_definitions/welcome_steps.rb
@@ -1,3 +1,7 @@
 Then(/^I should see the welcome message$/) do
   expect(page).to have_content("Modern Beauty")
 end
+
+Then(/^I should go to the About page$/) do
+  expect(page).to have_content("About Modern Beauty")
+end
